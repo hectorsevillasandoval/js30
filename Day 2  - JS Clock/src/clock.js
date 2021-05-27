f9a1e0a351fa028e3ddd3e3e23bd851e9e2e73f6
hsevilla <sevillasandoval.hector@gmail.com>
2021-05-26 18:34:15 -0600

    adding module

---
+++ Day 2  - JS Clock/src/clock.js
@@ -0,0 +1,11 @@
+/**
+ * Clock JS Class
+ */
+
+class ClockJs {
+    constructor(secondsHand, minutesHand, hoursHand) {
+        this.secondsHand = secondHand;
+        this.minutesHand = minutesHand;
+        this.hoursHand = hoursHand;
+    }
+}
